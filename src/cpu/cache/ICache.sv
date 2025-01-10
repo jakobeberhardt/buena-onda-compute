83bf3096c005cc907b72b8afbb08bf4bec8036d0
jakobeberhardt <jakob.eberhardt@mail.de>
2025-01-10 16:58:07 +0100

    Adds 5 cycle delay

--- src/cpu/cache/ICache.sv
+++ src/cpu/cache/ICache.sv
@@ -8,14 +8,35 @@ module ICache(
     input  logic [31:0] mem_dataOut
 );
 
-    assign mem_addr  = addr_in;  
+    assign mem_addr  = addr_in;
     assign data_out  = mem_dataOut;
-    assign iCache_stall = 0;
+    localparam int DELAY_CYCLES = 5;
+    logic [31:0] prev_addr;
+
+    logic [$clog2(DELAY_CYCLES+1)-1:0] stall_counter;
+    logic stall_reg;
+    assign iCache_stall = stall_reg;
 
     always_ff @(posedge clock or posedge reset) begin
         if (reset) begin
+            prev_addr      <= '0;
+            stall_counter  <= '0;
+            stall_reg      <= 1'b0;
         end
         else begin
+            if (addr_in != prev_addr) begin
+                stall_reg      <= 1'b1;
+                stall_counter  <= DELAY_CYCLES;
+                prev_addr      <= addr_in;
+            end
+            else begin
+                if (stall_counter > 0) begin
+                    stall_counter <= stall_counter - 1'b1;
+                end
+                if (stall_counter == 1) begin
+                    stall_reg <= 1'b0;
+                end
+            end
         end
     end
 
